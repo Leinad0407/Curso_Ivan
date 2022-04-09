d23ad33a783d903962a6e40911c89ef50d39b39d
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-04-08 19:35:06 -0500

    Se añaden la tarea

--- clase_3/JS/index.js
+++ clase_3/JS/index.js
@@ -1,6 +1,6 @@
 alert ("Estoy conectado")
 
-
+/*
 //Ejercicios
 //Ejercicio 1
  let letra1 =  prompt('Ingrese una letra');
@@ -76,3 +76,166 @@ alert ("Estoy conectado")
     
 //     //Luna = (Peso en la Tierra/9,81m/s2) * 1,622m/s2
 // }
+
+*/
+
+/************************************************************************** */
+
+/* Ejercicios de práctica
+1- Evaluar una calificacion
+-Solicitar una calificacion al usuario de 0 a 100
+-Evaluemos con un if si su calificacions es A, B, C, D, E, etc
+-Mostremos con un switch su calificacion.
+	
+Calificaciónes
+< 60  calificacion 'F'
+< 70 calificacion 'D'
+< 80 calificacion C
+< 90 calificacion B
+< 100 calificacion A */
+
+/*console.log("Elige alguna de las siguientes opciones")
+console.log("1.- Mi calificación esta entre 90 y 100")
+console.log("2.- Mi calificación esta entre 80 y 90")
+console.log("3.- Mi calificación esta entre 70 y 80")
+console.log("4.- Mi calificación esta entre 60 y 70")
+console.log("5.- Mi calificación es menor de 60")
+const calificacion= prompt("Introduce el caso que describa mejor tu situación");
+console.log("La opcion introducidad es: ", calificacion)
+switch (calificacion){
+    case "1":
+        console.log("Tienes A, eres un orgullo para tu nación");
+        break;
+    case "2":
+        console.log("Tiene B, es un orgullo para su nación!");
+        break;
+    case "3":
+        console.log("Tiene C");
+        break;
+    case "4":
+        console.log("Tiene D");
+        break;
+    case "5":
+        console.log("Tiene F");
+        break;
+    default:
+        console.log("Solo intriduce el porcentaje en un rango del 0 al 100");
+        break;
+
+} */
+
+/*--------------------------------*/
+
+/* Ejercicios de práctica
+1- Evaluar una calificacion
+-Solicitar una calificacion al usuario de 0 a 100
+-Evaluemos con un if si su calificacions es A, B, C, D, E, etc
+-Mostremos con un switch su calificacion.
+	
+Calificaciónes
+< 60  calificacion 'F'
+< 70 calificacion 'D'
+< 80 calificacion C
+< 90 calificacion B
+< 100 calificacion A */
+
+/*const calificacion=Number(prompt("Introduce tu puntaje"))
+console.log("La calificaciion introducida es: ", calificacion);
+
+function miCalificacion(calificacion){
+    if(calificacion>=90 && calificacion<=100){
+        return "1"
+    }else if(calificacion>=80 && calificacion<90){
+        return "2"
+    }else if(calificacion>=70 && calificacion<80){
+        return "3"
+    }else if(calificacion>=60 && calificacion<70){
+        return "4"
+    }else if(calificacion>=0 && calificacion<60){
+        return "5"
+    }else{
+        return "Introduce solo una numeracion del 1 al 100"
+    }
+}
+miCalificacion(calificacion);
+
+opcionCalificacion=miCalificacion(calificacion);
+
+switch (opcionCalificacion){
+    case "1":
+        console.log("Tienes A, eres un orgullo para tu nación");
+        break;
+    case "2":
+        console.log("Tiene B, es un orgullo para su nación!");
+        break;
+    case "3":
+        console.log("Tiene C");
+        break;
+    case "4":
+        console.log("Tiene D");
+        break;
+    case "5":
+        console.log("Tiene F");
+        break;
+    default:
+        console.log("Solo intriduce el porcentaje en un rango del 0 al 100");
+        break;
+
+}
+
+
+
+
+
+
+
+
+/*--------------------------------------------*/
+
+/*2- Casa de cambio
+
+Crear una funcion que obtenga  una cantidad de  dinero a traves de prompt(), tambien debe  pedirle el nombre de un pais , investigar su moneda y el tipo de cambio para hacer la conversion de  la cantidad capturada. ejemplo: 
+
+1-¿Que cantidad en MXN deseas convertir ?
+R= 1500 MXN
+2¿A que pais vas a viajar?
+R= USA
+
+salida de la funcion: '$1500MXN (moneda local elegida por el usuario) equivale a $750USD(moneda del pais ingresado por el usuario)'
+
+Considerar minimo 4 tipo de monedas
+
+output:  '100 MXN equivale a 50 USD' */
+
+console.log("Estoy en méxico y viajaré a: elige una de las opciones:");
+console.log("1.- Estados Unidos");
+console.log("2.- Canada");
+console.log("3.- Brazil");
+console.log("4.- Venecia");
+
+/*Equivalencia en pesos mexicanos*/
+/*1 peso=0.050 UDS
+  1 peso = 0.063 dlres cadadienses
+  1 peso=0.23 reales brazileños
+  1 peso = 0.46 euros*/
+
+const pais=prompt("Ingresa al país que te diriges acorde al menu")
+const opcion=pais;
+
+
+function cambio(opcion){
+    if (pais==="1"){
+        let suDinero=Number(prompt("Introduce la cantidad de dinero que llevaras"))
+        let equivalencia=suDinero*0.050
+        console.log=("La cantidad ingresada es:", suDinero, "mexicanos y el equivalente es: ", equivalencia, "dolares Estado Unidenses")
+    }else if (pais==="2"){
+        let suDinero=Number(prompt("Introduce la cantidad de dinero que llevaras"))
+        let equivalencia=suDinero*0.063
+        console.log=("La cantidad equivalente es:", equivalencia);
+    }else if (pais==="3"){ suDinero=Number(prompt("Introduce la cantidad de dinero que llevaras"))
+    equivalencia=suDinero*0.023
+    console.log=("La cantidad ingresada es:", suDinero, "mexicanos y el equivalente es: ", equivalencia, "reales Brazileños")
+    }
+}
+
+cambio(opcion);
