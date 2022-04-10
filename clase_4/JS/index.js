2d8c399281c1a506bc08270353b03598b3eabd2f
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-04-10 17:42:23 -0500

    Se añade parte del ejercicio

--- clase_4/JS/index.js
+++ clase_4/JS/index.js
@@ -1,7 +1,7 @@
 alert ("Conectado ando")
 
 /*Ejercicio 1:
-    Solicitar el nombre completo de un usuario e indicarle cuantos caracteres tiene su nombre */
+    Solicitar el nombre completo de un usuario e indicarle cuantos caracteres tiene su nombre 
 
     const nombreCompleto=prompt("Ingrese su nombre completo");
 
@@ -9,15 +9,20 @@ alert ("Conectado ando")
         let laLongitudNombre=algunNombre;
         console.log(laLongitudNombre.length);
     }
-    longitudNombre(nombreCompleto)
+    longitudNombre(nombreCompleto) */
 
-    /*
-Ejercicio 2:
-    Solicitar el nombre completo a un usuario e indicarle cuantas vocales tiene.
-*/
+/* Ejercicio 2:
+    Solicitar el nombre completo a un usuario e indicarle cuantas vocales tiene. */
+    let nombreCompleto=prompt("Ingresa tu nombre completo")
+    let nombre =nombreCompleto;
+    let vocalA = nombre.match(/a/gi);
+    let vocalE = nombre.match(/e/gi);
+    let vocalI = nombre.match(/i/gi);
 
-let text = prompt("Ingresa tu nombre completo");
-text.match(/a/g);
-console.log(text.match(/a/g))
+    let numeroVocalA = vocalA.length;
+    let numeroVocalE = vocalE.length;
+    let numeroVocalI = vocalI.length;
+   
 
-
+    let total=numeroVocalA+numeroVocalE+numeroVocalI;
+    console.log("En total su nombre contiene", total, "vocales")
