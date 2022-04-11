55169fe8a5762d35e778fe0f7404508ca5a526d7
Daniel Aguilar <ing.aguilardan@gmail.com>
2022-04-10 20:09:03 -0500

    Se terminan los ejercicios

--- clase_4/JS/index.js
+++ clase_4/JS/index.js
@@ -13,16 +13,59 @@ alert ("Conectado ando")
 
 /* Ejercicio 2:
     Solicitar el nombre completo a un usuario e indicarle cuantas vocales tiene. */
+    let numerovocalA=0
+    let numerovocalE=0
+    let numerovocalI=0
+    let numerovocalO=0
+    let numerovocalU=0
+    
     let nombreCompleto=prompt("Ingresa tu nombre completo")
     let nombre =nombreCompleto;
+
+    function vocalesEnTuNombre(){
+        let vocalO = nombre.match(/o/ig);
     let vocalA = nombre.match(/a/gi);
     let vocalE = nombre.match(/e/gi);
     let vocalI = nombre.match(/i/gi);
+    let vocalU = nombre.match(/u/gi);
 
-    let numeroVocalA = vocalA.length;
-    let numeroVocalE = vocalE.length;
-    let numeroVocalI = vocalI.length;
-   
+    if(vocalA===null){
+        numerovocalA=0;
+    }else{
+        numerovocalA=vocalA.length
+    }
+    console.log("Hay ", numerovocalA, "vocales A")
 
-    let total=numeroVocalA+numeroVocalE+numeroVocalI;
-    console.log("En total su nombre contiene", total, "vocales")
+    if(vocalE===null){
+        numerovocalE=0;
+    }else{
+        numerovocalE=vocalE.length
+    }
+    console.log("Hay ", numerovocalE, "vocales E")
+    
+    if(vocalI===null){
+        numerovocalI=0;
+    }else{
+        numerovocalI=vocalI.length
+    }
+    console.log("Hay ", numerovocalI, "vocales I")
+
+    if(vocalO===null){
+        numerovocalO=0;
+    }else{
+        numerovocalO=vocalO.length
+    }
+    console.log("Hay ", numerovocalO, "vocales O")
+
+    if(vocalU===null){
+        numerovocalU=0;
+    }else{
+        numerovocalU=vocalU.length
+    }
+    console.log("Hay ", numerovocalU, "vocales U")
+
+    let total=numerovocalA + numerovocalE + numerovocalI + numerovocalO + numerovocalU;
+
+    console.log(" El total de las vocales en tu nombre es: ", total, "vocales")
+    }
+    vocalesEnTuNombre()
